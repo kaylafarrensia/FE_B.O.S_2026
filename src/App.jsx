4ae52466ad2d9a8c62f51fc322e5b83badb10750
Elicia Tjio <167668883+elicia8@users.noreply.github.com>
2026-07-22 00:39:46 +0700

    feat: add routes for overview, users, and japres pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import ComingSoon from './pages/ComingSoon/ComingSoon.jsx'
+import Dashboard from './pages/Admin/Dashboard.jsx'
+import Overview from './pages/Admin/Overview.jsx'
+import Japres from './pages/Admin/Japres.jsx'
+import User from './pages/Admin/Users.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ComingSoon />} />
+      <Route path="/admin" element={<Dashboard />}>
+        <Route index element={<Overview />} />
+        <Route path="overview" element={<Overview />} />
+        <Route path="users" element={<User />} />
+        <Route path="japres" element={<Japres />} />
+      </Route>
       {/* nanti tambah route lain di sini, misal: */}
       {/* <Route path="/about" element={<About />} /> */}
     </Routes>
