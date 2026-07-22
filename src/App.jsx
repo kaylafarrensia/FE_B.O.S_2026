bf122657634c40717f8bc43cbd323fa79fd97d71
Elicia Tjio <167668883+elicia8@users.noreply.github.com>
2026-07-22 22:50:36 +0700

    feat: add routes for documents, payment, and subscription pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Dashboard from './pages/Admin/Dashboard.jsx'
 import Overview from './pages/Admin/Overview.jsx'
 import Japres from './pages/Admin/Japres.jsx'
 import User from './pages/Admin/Users.jsx'
+import Documents from './pages/Admin/Documents.jsx'
+import Payment from './pages/Admin/Payment.jsx'
+import Subscription from './pages/Admin/Subscription.jsx'
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
         <Route index element={<Overview />} />
         <Route path="overview" element={<Overview />} />
         <Route path="users" element={<User />} />
+        <Route path="documents" element={<Documents />} />
+        <Route path="payment" element={<Payment />} />
         <Route path="japres" element={<Japres />} />
+        <Route path="subscription" element={<Subscription />} />
       </Route>
       {/* nanti tambah route lain di sini, misal: */}
       {/* <Route path="/about" element={<About />} /> */}
